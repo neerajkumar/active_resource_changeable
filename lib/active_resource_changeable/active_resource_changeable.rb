ebbc84d522ef5a42852389665d2cc01cf5542e3a
Neeraj Kumar <neeraj.kumar@gmail.com>
2016-11-22 00:13:35 +0530

    some more change in logic

--- lib/active_resource_changeable/active_resource_changeable.rb
+++ lib/active_resource_changeable/active_resource_changeable.rb
@@ -15,16 +15,19 @@ module ActiveResourceChangeable
     hash = _self.attributes
     hash.each do |key, val|
       if val.is_a? ActiveResource::Base
-        attr_changes = prechanges(val, original_objekt.send(key), keys.push(key))
+        prechanges(val, original_objekt.send(key), keys.push(key))
         keys.pop
       elsif original_objekt.send(key) != val
         temp_hash = HashWithIndifferentAccess.new
         if keys.present?
           keys.each_with_index do |kkey, i|
-            temp_hash = { "#{keys[i]}": "#{keys[i + 1]}"}
+            if temp_hash[keys[i]].present?
+              temp_hash[keys[i]].merge!({"#{keys[i+1]}": {"#{key}": [original_objekt.send(key), val]}})
+            else
+              temp_hash[keys[i]] = {"#{keys[i+1]}": {"#{key}": [original_objekt.send(key), val]}}
+            end
           end
-          temp_hash[keys[keys.length - 1]] = {"#{key}": [original_objekt.send(key), val]}
-          @@hash.merge!(temp_hash)
+          @@hash = @@hash.deep_merge(temp_hash)
         else
           @@hash.merge!("#{key}": [original_objekt.send(key), val])
         end
